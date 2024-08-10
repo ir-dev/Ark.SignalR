a500e18b12400449efc3ca4cc78a6de19ad5cd0d
Immanuel Raj <kimr@solar.dk>
2024-08-10 20:00:16 +0530

    base changes.

---
+++ Ark.SignalR/Ark.SignalR/ArkSignalHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Ark.SignalR
+{
+    public class ArkSignalHub : Hub
+    {
+        public virtual async Task InvokeEventAsync(string evt, string content)
+        {
+            await Clients.All.SendAsync(evt, content);
+        }
+    }
+}
---
+++ Ark.SignalR/Ark.SignalR/SignalRExtn.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Ark.SignalR
+{
+    public static class ArkSignalRExtn
+    {
+        public static void AddArkSignalR(this IServiceCollection services)
+        {
+            services.AddArkSignalR(new HubOptions() { EnableDetailedErrors = true });
+        }
+        public static void AddArkSignalR(this IServiceCollection services, HubOptions opts)
+        {
+            services
+                .AddSignalR(opt =>
+                {
+                    opt = opts;
+                })
+                .AddNewtonsoftJsonProtocol(oo =>
+                {
+                    oo.PayloadSerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+            services.AddSingleton<SignalR.ArkSignalHub>();
+        }
+        public static void UseArkSignalR(this IApplicationBuilder app, string pattern)
+        {
+            app.UseArkSignalR<SignalR.ArkSignalHub>(pattern);
+        }
+        public static void UseArkSignalR<T>(this IApplicationBuilder app, string pattern) where T : Hub
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<T>($"/{pattern}");
+            });
+        }
+    }
+}
